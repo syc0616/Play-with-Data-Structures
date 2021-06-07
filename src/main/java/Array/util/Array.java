daa32248944f7222610fee4ef7e6679995321e12
syc0616 <106982036@qq.com>
2021-06-07 14:31:41 +0800

    Default Changelist

--- src/main/java/Array/util/Array.java
+++ src/main/java/Array/util/Array.java
@@ -1,14 +1,21 @@
 package Array.util;
 
-public class Array {
+
+/*
+    泛型：
+    让数据结构中可放置任何数据类型；
+    不能是基本数据类型，只能是类对象
+
+ */
+public class Array<E> {
     // 数组数据
-    private int[] data;
+    private E[] data;
     // 数组实际数据大小
     private int size;
 
     // 构造函数，传入数组的容量capacity构造Array
     public Array(int capacity){
-        data = new int[capacity];
+        data = (E[])new Object[capacity];
         size = 0;
     }
 
@@ -22,30 +29,35 @@ public class Array {
         return data.length;
     }
 
+    // 获取数组中的元素个数
     public int getSize(){
         return size;
     }
 
+    // 返回数组是否为空
     public boolean isEmpty(){
         return size == 0;
     }
 
-    public void addLast(int e){
+    // 向所有元素后添加一个新元素
+    public void addLast(E e){
         add(size, e);
     }
 
-    public void addFirst(int e){
+    // 在所有元素前添加一个新元素
+    public void addFirst(E e){
         add(0, e);
     }
 
-    public void add(int index, int e){
-        // 数组已满
-        if(size == data.length){
-            throw new IllegalArgumentException("Add failed. Array is full.");
-        }
-
+    // 在index索引的位置插入一个新元素e
+    public void add(int index, E e){
         if(index < 0 || index > size){
             throw new IllegalArgumentException("Add failed. Require index >= 0 and index <= size.");
+        }
+
+        // 数组已满，重新分配
+        if(size == data.length){
+            resize(2 * data.length);
         }
 
         // 从最后一个元素开始向后移动，空出index位置，供插入
@@ -58,19 +70,94 @@ public class Array {
         size++;
     }
 
-    public int get(int index){
+    // 获取index索引位置的元素
+    public E get(int index){
         if (index < 0 || index >= size){
             throw new IllegalArgumentException("Get failed. Index is illegal.");
         }
         return data[index];
     }
 
-    public void set(int index, int e){
+    // 修改index索引位置的元素为e
+    public void set(int index, E e){
         if (index < 0 || index >= size){
             throw new IllegalArgumentException("Get failed. Index is illegal.");
         }
 
         data[index] = e;
+    }
+
+    // 查找数组中是否有元素e
+    public boolean contains(E e){
+        for(int i = 0; i < data.length; i++){
+            if (data[i] == e){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // 查找数组中元素e所在的索引，若不存在元素e，则返回-1
+    public int find(E e){
+        for (int i = 0; i < data.length; i++){
+            if (data[i] == e){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    // 从数组中删除index位置的元素，返回删除的元素
+    public E remove(int index){
+        if (index < 0 || index >= size){
+            throw new IllegalArgumentException("Remove failed. Index is illegal.");
+        }
+
+        // 将元素后移
+        E ret = data[index];
+        for (int i = index + 1; i < size; i++){
+            data[i - 1] = data[i];
+        }
+
+        size--;
+
+        // loitering objects != memory leak
+        data[size] = null;
+
+        // 数组大小为原理的1/4；将数组大小调整为原来的1/2
+        if (size == data.length / 4 && data.length / 2 != 0){
+            resize(data.length / 2);
+        }
+
+        return ret;
+    }
+
+    // 从数组中删除第一个元素
+    public E removeFirst(){
+        return remove(0);
+    }
+
+    // 从数组中删除最后一个元素
+    public E removeLast(){
+        return remove(size - 1);
+    }
+
+    // 从数组中删除元素e
+    public void removeElement(E e){
+        int index = find(e);
+        if (index != -1){
+            remove(index);
+        }
+    }
+
+    private void resize(int newCapacity){
+        E[] newData = (E[])new Object[newCapacity];
+        // 将旧值拷贝新的数组
+        for (int i = 0; i < size; i++){
+            newData[i] = data[i];
+        }
+        data = newData;
+
     }
 
     @Override
